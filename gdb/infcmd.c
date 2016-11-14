4ee773ddb3334393e0912c21db660a5d4a6f0ba4
Palmer Dabbelt <palmer@dabbelt.com>
2016-11-14 11:37:43 -0800

    Revert what I assume was a mis-merge

--- gdb/infcmd.c
+++ gdb/infcmd.c
@@ -3439,9 +3439,8 @@ otherwise all the threads in the program are stopped.  To \n\
 interrupt all running threads in non-stop mode, use the -a option."));
 
   c = add_info ("registers", nofp_registers_info, _("\
-Display registers and their contents, for the selected stack frame.\n\
-Usage: info registers [all|register|register group] ...\n\
-By default, the general register group will be shown."));
+List of integer registers and their contents, for selected stack frame.\n\
+Register name as argument means describe only that register."));
   add_info_alias ("r", "registers", 1);
   set_cmd_completer (c, reg_or_group_completer);
 
