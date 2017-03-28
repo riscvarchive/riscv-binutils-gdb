ed7721dd8f5823ccc4508047cef5574421406025
Palmer Dabbelt <palmer@dabbelt.com>
2017-03-28 15:35:47 -0700

    Archive

--- gas/testsuite/gas/mips/mips16-relax-unextended-1.d
+++
@@ -1,3 +0,0 @@
-#name: MIPS16 relaxation with unextended instructions forced 1
-#as: -32
-#error-output: mips16-relax-unextended-1.l
--- sim/riscv/tconfig.h
+++
@@ -1,4 +0,0 @@
-/* RISC-V target configuration file.  -*- C -*- */
-
-/* ??? Temporary hack until model support unified.  */
-#define SIM_HAVE_MODEL
