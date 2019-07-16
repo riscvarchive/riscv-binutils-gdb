64bbdbeeddcd204d50c72b5601e32feeb88f2af7
Jim Wilson <jimw@sifive.com>
2019-07-16 13:46:30 -0700

    Updates to AMO instructions.

--- gas/testsuite/gas/riscv/vector-insns.s
+++ gas/testsuite/gas/riscv/vector-insns.s
@@ -1009,78 +1009,78 @@
 
 	vamoaddw.v v4, v8, (a1), v4
 	vamoaddw.v x0, v8, (a1), v4
-	vamoaddd.v v4, v8, (a1), v4
-	vamoaddd.v x0, v8, (a1), v4
+	vamoadde.v v4, v8, (a1), v4
+	vamoadde.v x0, v8, (a1), v4
 	vamoaddw.v v4, v8, (a1), v4, v0.t
 	vamoaddw.v x0, v8, (a1), v4, v0.t
-	vamoaddd.v v4, v8, (a1), v4, v0.t
-	vamoaddd.v x0, v8, (a1), v4, v0.t
+	vamoadde.v v4, v8, (a1), v4, v0.t
+	vamoadde.v x0, v8, (a1), v4, v0.t
 	vamoswapw.v v4, v8, (a1), v4
 	vamoswapw.v x0, v8, (a1), v4
-	vamoswapd.v v4, v8, (a1), v4
-	vamoswapd.v x0, v8, (a1), v4
+	vamoswape.v v4, v8, (a1), v4
+	vamoswape.v x0, v8, (a1), v4
 	vamoswapw.v v4, v8, (a1), v4, v0.t
 	vamoswapw.v x0, v8, (a1), v4, v0.t
-	vamoswapd.v v4, v8, (a1), v4, v0.t
-	vamoswapd.v x0, v8, (a1), v4, v0.t
+	vamoswape.v v4, v8, (a1), v4, v0.t
+	vamoswape.v x0, v8, (a1), v4, v0.t
 
 	vamoxorw.v v4, v8, (a1), v4
 	vamoxorw.v x0, v8, (a1), v4
-	vamoxord.v v4, v8, (a1), v4
-	vamoxord.v x0, v8, (a1), v4
+	vamoxore.v v4, v8, (a1), v4
+	vamoxore.v x0, v8, (a1), v4
 	vamoxorw.v v4, v8, (a1), v4, v0.t
 	vamoxorw.v x0, v8, (a1), v4, v0.t
-	vamoxord.v v4, v8, (a1), v4, v0.t
-	vamoxord.v x0, v8, (a1), v4, v0.t
+	vamoxore.v v4, v8, (a1), v4, v0.t
+	vamoxore.v x0, v8, (a1), v4, v0.t
 	vamoandw.v v4, v8, (a1), v4
 	vamoandw.v x0, v8, (a1), v4
-	vamoandd.v v4, v8, (a1), v4
-	vamoandd.v x0, v8, (a1), v4
+	vamoande.v v4, v8, (a1), v4
+	vamoande.v x0, v8, (a1), v4
 	vamoandw.v v4, v8, (a1), v4, v0.t
 	vamoandw.v x0, v8, (a1), v4, v0.t
-	vamoandd.v v4, v8, (a1), v4, v0.t
-	vamoandd.v x0, v8, (a1), v4, v0.t
+	vamoande.v v4, v8, (a1), v4, v0.t
+	vamoande.v x0, v8, (a1), v4, v0.t
 	vamoorw.v v4, v8, (a1), v4
 	vamoorw.v x0, v8, (a1), v4
-	vamoord.v v4, v8, (a1), v4
-	vamoord.v x0, v8, (a1), v4
+	vamoore.v v4, v8, (a1), v4
+	vamoore.v x0, v8, (a1), v4
 	vamoorw.v v4, v8, (a1), v4, v0.t
 	vamoorw.v x0, v8, (a1), v4, v0.t
-	vamoord.v v4, v8, (a1), v4, v0.t
-	vamoord.v x0, v8, (a1), v4, v0.t
+	vamoore.v v4, v8, (a1), v4, v0.t
+	vamoore.v x0, v8, (a1), v4, v0.t
 
 	vamominw.v v4, v8, (a1), v4
 	vamominw.v x0, v8, (a1), v4
-	vamomind.v v4, v8, (a1), v4
-	vamomind.v x0, v8, (a1), v4
+	vamomine.v v4, v8, (a1), v4
+	vamomine.v x0, v8, (a1), v4
 	vamominw.v v4, v8, (a1), v4, v0.t
 	vamominw.v x0, v8, (a1), v4, v0.t
-	vamomind.v v4, v8, (a1), v4, v0.t
-	vamomind.v x0, v8, (a1), v4, v0.t
+	vamomine.v v4, v8, (a1), v4, v0.t
+	vamomine.v x0, v8, (a1), v4, v0.t
 	vamomaxw.v v4, v8, (a1), v4
 	vamomaxw.v x0, v8, (a1), v4
-	vamomaxd.v v4, v8, (a1), v4
-	vamomaxd.v x0, v8, (a1), v4
+	vamomaxe.v v4, v8, (a1), v4
+	vamomaxe.v x0, v8, (a1), v4
 	vamomaxw.v v4, v8, (a1), v4, v0.t
 	vamomaxw.v x0, v8, (a1), v4, v0.t
-	vamomaxd.v v4, v8, (a1), v4, v0.t
-	vamomaxd.v x0, v8, (a1), v4, v0.t
+	vamomaxe.v v4, v8, (a1), v4, v0.t
+	vamomaxe.v x0, v8, (a1), v4, v0.t
 	vamominuw.v v4, v8, (a1), v4
 	vamominuw.v x0, v8, (a1), v4
-	vamominud.v v4, v8, (a1), v4
-	vamominud.v x0, v8, (a1), v4
+	vamominue.v v4, v8, (a1), v4
+	vamominue.v x0, v8, (a1), v4
 	vamominuw.v v4, v8, (a1), v4, v0.t
 	vamominuw.v x0, v8, (a1), v4, v0.t
-	vamominud.v v4, v8, (a1), v4, v0.t
-	vamominud.v x0, v8, (a1), v4, v0.t
+	vamominue.v v4, v8, (a1), v4, v0.t
+	vamominue.v x0, v8, (a1), v4, v0.t
 	vamomaxuw.v v4, v8, (a1), v4
 	vamomaxuw.v x0, v8, (a1), v4
-	vamomaxud.v v4, v8, (a1), v4
-	vamomaxud.v x0, v8, (a1), v4
+	vamomaxue.v v4, v8, (a1), v4
+	vamomaxue.v x0, v8, (a1), v4
 	vamomaxuw.v v4, v8, (a1), v4, v0.t
 	vamomaxuw.v x0, v8, (a1), v4, v0.t
-	vamomaxud.v v4, v8, (a1), v4, v0.t
-	vamomaxud.v x0, v8, (a1), v4, v0.t
+	vamomaxue.v v4, v8, (a1), v4, v0.t
+	vamomaxue.v x0, v8, (a1), v4, v0.t
 
 	vadd.vv v4, v8, v12
 	vadd.vx v4, v8, a1
