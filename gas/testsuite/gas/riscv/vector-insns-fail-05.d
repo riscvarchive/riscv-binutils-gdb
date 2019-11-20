81cd2e16e34be1dc5beebd0827384cc9026fd490
Nelson Chu <nelson.chu@sifive.com>
2019-11-20 16:57:48 +0800

    Support the vector instruction constraints.
    Currently, assembler is hard to know what the LMUL exactly is when assembling,
    therefore we can just check the part of the constraints in the static
    assemble-time.
    
    Another thing is that we just report "Error: illegal operands" messages for all
    vector constraints now.  It would be nice if we can report the clearer erorr
    message, so this may be the next stuff we can improve.
    
    * Widening and narrowing vector arithmetic instructions
    
    Assume the LMUL is 1, so at least vd can not be the same as vs1, vs2 and vm.
    Otherwise the an illegal instruction exception is raised.  However, the
    widening vector register should be at least a double-width group that contains
    two register ([v0, v1], [v2, v3], ..., [v30, v31]). So that if the double-width
    vd is v2, then vs1, vs2 and vm can not be v2 and v3 if their size are SEW
    rather than 2SEW.  The narrowing vector instructions are the same as the
    widening ones, except their widening vector register is vs2 rather than vd.
    
    Beside, the widening vector reduction instructions are special.  The scalar
    input and output operands are held in element 0 of a single vector register,
    not a vector register group, so any vector register can be the scalar source
    or destination of a vector reduction regardless of LMUL setting.  Therefore,
    we don’t need to check the overlap for the widening vector reduction
    instruction (vwredsumu.vs, vwredsum.vs, vfwredosum.vs, vfwredsum.vs).
    
    For widening vector instructions, the destination vector register group cannot
    overlap a source vector register group of a different element width (including
    the mask register if masked).  For the narrowing instructions, the destination
    vector register group cannot overlap the first source vector register group
    (specified by vs2).
    
    * Vector indexed segment loads and stores
    
    The destination vector register groups cannot overlap the source vector register
    group (specified by vs2), nor can they overlap the mask register if masked.
    
    * Vector Iota
    
    The destination vector register group cannot overlap the source vector mask
    register.  If the instruction is masked, an illegal instruction exception is
    issued if the destination vector register group overlaps v0.
    
    * Vector slideup and slide1up
    
    The destination vector register group cannot overlap the vector register group
    of the source vector register group or the mask register,
    
    * Vector register gather and compress
    
    The destination vector register group cannot overlap with the source vector
    register groups, including the mask register if the operation is masked.

---
+++ gas/testsuite/gas/riscv/vector-insns-fail-05.d
@@ -0,0 +1,3 @@
+#as: -march=rv32ifv
+#source: vector-insns-fail-05.s
+#error_output: vector-insns-fail-05.l
---
+++ gas/testsuite/gas/riscv/vector-insns-fail-06.d
@@ -0,0 +1,3 @@
+#as: -march=rv32ifv
+#source: vector-insns-fail-06.s
+#error_output: vector-insns-fail-06.l
