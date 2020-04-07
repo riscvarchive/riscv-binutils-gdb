87845cd6f5fed2e01980dc072ffdb612fcc3e04c
Nelson Chu <nelson.chu@sifive.com>
2020-04-07 16:58:32 +0800

    Change the expansions for vmsge.vx and vmsgeu.vx with the unmasked va.

--- gas/testsuite/gas/riscv/vector-insns.d
+++ gas/testsuite/gas/riscv/vector-insns.d
@@ -604,9 +604,9 @@ Disassembly of section .text:
 [ 	]+[0-9a-f]+:[ 	]+7887b257[ 	]+vmsgtu.vi[ 	]+v4,v8,15,v0.t
 [ 	]+[0-9a-f]+:[ 	]+78883257[ 	]+vmsgtu.vi[ 	]+v4,v8,-16,v0.t
 [ 	]+[0-9a-f]+:[ 	]+6e85c257[ 	]+vmslt.vx[ 	]+v4,v8,a1
-[ 	]+[0-9a-f]+:[ 	]+7685a257[ 	]+vmnand.mm[ 	]+v4,v8,v11
+[ 	]+[0-9a-f]+:[ 	]+76422257[ 	]+vmnot.m[ 	]+v4,v4
 [ 	]+[0-9a-f]+:[ 	]+6a85c257[ 	]+vmsltu.vx[ 	]+v4,v8,a1
-[ 	]+[0-9a-f]+:[ 	]+7685a257[ 	]+vmnand.mm[ 	]+v4,v8,v11
+[ 	]+[0-9a-f]+:[ 	]+76422257[ 	]+vmnot.m[ 	]+v4,v4
 [ 	]+[0-9a-f]+:[ 	]+6cc64457[ 	]+vmslt.vx[ 	]+v8,v12,a2,v0.t
 [ 	]+[0-9a-f]+:[ 	]+6e802457[ 	]+vmxor.mm[ 	]+v8,v8,v0
 [ 	]+[0-9a-f]+:[ 	]+68c64457[ 	]+vmsltu.vx[ 	]+v8,v12,a2,v0.t
