ac1b3acb46df786f8f2b01128e8e5e556eb3c221
Nelson Chu <nelson.chu@sifive.com>
2020-04-09 09:49:48 +0800

    Add overlap constraint for segment load destination and mask register.
    This overlap constraint is only needed for the segment loads, the segmant
    stores are fine.

--- gas/testsuite/gas/riscv/vector-insns-fail-01.s
+++ gas/testsuite/gas/riscv/vector-insns-fail-01.s
@@ -1,3 +1,91 @@
+# Vector Unit-Stride Segment Loads and Stores
+
+	vlseg2e.v v0, (a0), v0.t	# vd overlap vm
+	vlseg2e.v v4, (a0), v0.t	# OK
+	vsseg2e.v v0, (a0), v0.t	# OK
+	vsseg2e.v v4, (a0), v0.t	# OK
+	vlseg2eff.v v0, (a0), v0.t	# vd overlap vm
+	vlseg2eff.v v4, (a0), v0.t	# OK
+
+	vlseg3e.v v0, (a0), v0.t	# vd overlap vm
+	vlseg3e.v v4, (a0), v0.t	# OK
+	vsseg3e.v v0, (a0), v0.t	# OK
+	vsseg3e.v v4, (a0), v0.t	# OK
+	vlseg3eff.v v0, (a0), v0.t	# vd overlap vm
+	vlseg3eff.v v4, (a0), v0.t	# OK
+
+	vlseg4e.v v0, (a0), v0.t	# vd overlap vm
+	vlseg4e.v v4, (a0), v0.t	# OK
+	vsseg4e.v v0, (a0), v0.t	# OK
+	vsseg4e.v v4, (a0), v0.t	# OK
+	vlseg4eff.v v0, (a0), v0.t	# vd overlap vm
+	vlseg4eff.v v4, (a0), v0.t	# OK
+
+	vlseg5e.v v0, (a0), v0.t	# vd overlap vm
+	vlseg5e.v v4, (a0), v0.t	# OK
+	vsseg5e.v v0, (a0), v0.t	# OK
+	vsseg5e.v v4, (a0), v0.t	# OK
+	vlseg5eff.v v0, (a0), v0.t	# vd overlap vm
+	vlseg5eff.v v4, (a0), v0.t	# OK
+
+	vlseg6e.v v0, (a0), v0.t	# vd overlap vm
+	vlseg6e.v v4, (a0), v0.t	# OK
+	vsseg6e.v v0, (a0), v0.t	# OK
+	vsseg6e.v v4, (a0), v0.t	# OK
+	vlseg6eff.v v0, (a0), v0.t	# vd overlap vm
+	vlseg6eff.v v4, (a0), v0.t	# OK
+
+	vlseg7e.v v0, (a0), v0.t	# vd overlap vm
+	vlseg7e.v v4, (a0), v0.t	# OK
+	vsseg7e.v v0, (a0), v0.t	# OK
+	vsseg7e.v v4, (a0), v0.t	# OK
+	vlseg7eff.v v0, (a0), v0.t	# vd overlap vm
+	vlseg7eff.v v4, (a0), v0.t	# OK
+
+	vlseg8e.v v0, (a0), v0.t	# vd overlap vm
+	vlseg8e.v v4, (a0), v0.t	# OK
+	vsseg8e.v v0, (a0), v0.t	# OK
+	vsseg8e.v v4, (a0), v0.t	# OK
+	vlseg8eff.v v0, (a0), v0.t	# vd overlap vm
+	vlseg8eff.v v4, (a0), v0.t	# OK
+
+# Vector Strided Segment Loads and Stores
+
+	vlsseg2e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vlsseg2e.v v4, (a0), a1, v0.t	# OK
+	vssseg2e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vssseg2e.v v4, (a0), a1, v0.t	# OK
+
+	vlsseg3e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vlsseg3e.v v4, (a0), a1, v0.t	# OK
+	vssseg3e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vssseg3e.v v4, (a0), a1, v0.t	# OK
+
+	vlsseg4e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vlsseg4e.v v4, (a0), a1, v0.t	# OK
+	vssseg4e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vssseg4e.v v4, (a0), a1, v0.t	# OK
+
+	vlsseg5e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vlsseg5e.v v4, (a0), a1, v0.t	# OK
+	vssseg5e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vssseg5e.v v4, (a0), a1, v0.t	# OK
+
+	vlsseg6e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vlsseg6e.v v4, (a0), a1, v0.t	# OK
+	vssseg6e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vssseg6e.v v4, (a0), a1, v0.t	# OK
+
+	vlsseg7e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vlsseg7e.v v4, (a0), a1, v0.t	# OK
+	vssseg7e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vssseg7e.v v4, (a0), a1, v0.t	# OK
+
+	vlsseg8e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vlsseg8e.v v4, (a0), a1, v0.t	# OK
+	vssseg8e.v v0, (a0), a1, v0.t	# vd overlap vm
+	vssseg8e.v v4, (a0), a1, v0.t	# OK
+
 # Vector Indexed Segment Loads and Stores
 
 	vlxseg2e.v v0, (a0), v2		# OK
