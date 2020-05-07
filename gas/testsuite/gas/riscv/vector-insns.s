861dfc9b3a031befb8472b94255c5f0d2e5d3cec
Nelson Chu <nelson.chu@sifive.com>
2020-05-07 14:45:00 +0800

    Add EEW VAMO instructions.

--- gas/testsuite/gas/riscv/vector-insns.s
+++ gas/testsuite/gas/riscv/vector-insns.s
@@ -1647,83 +1647,317 @@
 	vs1r.v v3, (a1)
 	vs1r.v v3, 0(a1)
 
-	vamoadde.v v4, (a1), v8, v4
-	vamoadde.v x0, (a1), v8, v4
-	vamoadde.v v4, (a1), v8, v4, v0.t
-	vamoadde.v x0, (a1), v8, v4, v0.t
-	vamoswape.v v4, (a1), v8, v4
-	vamoswape.v x0, (a1), v8, v4
-	vamoswape.v v4, (a1), v8, v4, v0.t
-	vamoswape.v x0, (a1), v8, v4, v0.t
+	vamoaddei8.v v4, (a1), v8, v4
+	vamoaddei8.v x0, (a1), v8, v4
+	vamoaddei8.v v4, (a1), v8, v4, v0.t
+	vamoaddei8.v x0, (a1), v8, v4, v0.t
+	vamoswapei8.v v4, (a1), v8, v4
+	vamoswapei8.v x0, (a1), v8, v4
+	vamoswapei8.v v4, (a1), v8, v4, v0.t
+	vamoswapei8.v x0, (a1), v8, v4, v0.t
 
-	vamoxore.v v4, (a1), v8, v4
-	vamoxore.v x0, (a1), v8, v4
-	vamoxore.v v4, (a1), v8, v4, v0.t
-	vamoxore.v x0, (a1), v8, v4, v0.t
-	vamoande.v v4, (a1), v8, v4
-	vamoande.v x0, (a1), v8, v4
-	vamoande.v v4, (a1), v8, v4, v0.t
-	vamoande.v x0, (a1), v8, v4, v0.t
-	vamoore.v v4, (a1), v8, v4
-	vamoore.v x0, (a1), v8, v4
-	vamoore.v v4, (a1), v8, v4, v0.t
-	vamoore.v x0, (a1), v8, v4, v0.t
+	vamoxorei8.v v4, (a1), v8, v4
+	vamoxorei8.v x0, (a1), v8, v4
+	vamoxorei8.v v4, (a1), v8, v4, v0.t
+	vamoxorei8.v x0, (a1), v8, v4, v0.t
+	vamoandei8.v v4, (a1), v8, v4
+	vamoandei8.v x0, (a1), v8, v4
+	vamoandei8.v v4, (a1), v8, v4, v0.t
+	vamoandei8.v x0, (a1), v8, v4, v0.t
+	vamoorei8.v v4, (a1), v8, v4
+	vamoorei8.v x0, (a1), v8, v4
+	vamoorei8.v v4, (a1), v8, v4, v0.t
+	vamoorei8.v x0, (a1), v8, v4, v0.t
 
-	vamomine.v v4, (a1), v8, v4
-	vamomine.v x0, (a1), v8, v4
-	vamomine.v v4, (a1), v8, v4, v0.t
-	vamomine.v x0, (a1), v8, v4, v0.t
-	vamomaxe.v v4, (a1), v8, v4
-	vamomaxe.v x0, (a1), v8, v4
-	vamomaxe.v v4, (a1), v8, v4, v0.t
-	vamomaxe.v x0, (a1), v8, v4, v0.t
-	vamominue.v v4, (a1), v8, v4
-	vamominue.v x0, (a1), v8, v4
-	vamominue.v v4, (a1), v8, v4, v0.t
-	vamominue.v x0, (a1), v8, v4, v0.t
-	vamomaxue.v v4, (a1), v8, v4
-	vamomaxue.v x0, (a1), v8, v4
-	vamomaxue.v v4, (a1), v8, v4, v0.t
-	vamomaxue.v x0, (a1), v8, v4, v0.t
+	vamominei8.v v4, (a1), v8, v4
+	vamominei8.v x0, (a1), v8, v4
+	vamominei8.v v4, (a1), v8, v4, v0.t
+	vamominei8.v x0, (a1), v8, v4, v0.t
+	vamomaxei8.v v4, (a1), v8, v4
+	vamomaxei8.v x0, (a1), v8, v4
+	vamomaxei8.v v4, (a1), v8, v4, v0.t
+	vamomaxei8.v x0, (a1), v8, v4, v0.t
+	vamominuei8.v v4, (a1), v8, v4
+	vamominuei8.v x0, (a1), v8, v4
+	vamominuei8.v v4, (a1), v8, v4, v0.t
+	vamominuei8.v x0, (a1), v8, v4, v0.t
+	vamomaxuei8.v v4, (a1), v8, v4
+	vamomaxuei8.v x0, (a1), v8, v4
+	vamomaxuei8.v v4, (a1), v8, v4, v0.t
+	vamomaxuei8.v x0, (a1), v8, v4, v0.t
 
-	vamoadde.v v4, 0(a1), v8, v4
-	vamoadde.v x0, 0(a1), v8, v4
-	vamoadde.v v4, 0(a1), v8, v4, v0.t
-	vamoadde.v x0, 0(a1), v8, v4, v0.t
-	vamoswape.v v4, 0(a1), v8, v4
-	vamoswape.v x0, 0(a1), v8, v4
-	vamoswape.v v4, 0(a1), v8, v4, v0.t
-	vamoswape.v x0, 0(a1), v8, v4, v0.t
+	vamoaddei8.v v4, 0(a1), v8, v4
+	vamoaddei8.v x0, 0(a1), v8, v4
+	vamoaddei8.v v4, 0(a1), v8, v4, v0.t
+	vamoaddei8.v x0, 0(a1), v8, v4, v0.t
+	vamoswapei8.v v4, 0(a1), v8, v4
+	vamoswapei8.v x0, 0(a1), v8, v4
+	vamoswapei8.v v4, 0(a1), v8, v4, v0.t
+	vamoswapei8.v x0, 0(a1), v8, v4, v0.t
 
-	vamoxore.v v4, 0(a1), v8, v4
-	vamoxore.v x0, 0(a1), v8, v4
-	vamoxore.v v4, 0(a1), v8, v4, v0.t
-	vamoxore.v x0, 0(a1), v8, v4, v0.t
-	vamoande.v v4, 0(a1), v8, v4
-	vamoande.v x0, 0(a1), v8, v4
-	vamoande.v v4, 0(a1), v8, v4, v0.t
-	vamoande.v x0, 0(a1), v8, v4, v0.t
-	vamoore.v v4, 0(a1), v8, v4
-	vamoore.v x0, 0(a1), v8, v4
-	vamoore.v v4, 0(a1), v8, v4, v0.t
-	vamoore.v x0, 0(a1), v8, v4, v0.t
+	vamoxorei8.v v4, 0(a1), v8, v4
+	vamoxorei8.v x0, 0(a1), v8, v4
+	vamoxorei8.v v4, 0(a1), v8, v4, v0.t
+	vamoxorei8.v x0, 0(a1), v8, v4, v0.t
+	vamoandei8.v v4, 0(a1), v8, v4
+	vamoandei8.v x0, 0(a1), v8, v4
+	vamoandei8.v v4, 0(a1), v8, v4, v0.t
+	vamoandei8.v x0, 0(a1), v8, v4, v0.t
+	vamoorei8.v v4, 0(a1), v8, v4
+	vamoorei8.v x0, 0(a1), v8, v4
+	vamoorei8.v v4, 0(a1), v8, v4, v0.t
+	vamoorei8.v x0, 0(a1), v8, v4, v0.t
 
-	vamomine.v v4, 0(a1), v8, v4
-	vamomine.v x0, 0(a1), v8, v4
-	vamomine.v v4, 0(a1), v8, v4, v0.t
-	vamomine.v x0, 0(a1), v8, v4, v0.t
-	vamomaxe.v v4, 0(a1), v8, v4
-	vamomaxe.v x0, 0(a1), v8, v4
-	vamomaxe.v v4, 0(a1), v8, v4, v0.t
-	vamomaxe.v x0, 0(a1), v8, v4, v0.t
-	vamominue.v v4, 0(a1), v8, v4
-	vamominue.v x0, 0(a1), v8, v4
-	vamominue.v v4, 0(a1), v8, v4, v0.t
-	vamominue.v x0, 0(a1), v8, v4, v0.t
-	vamomaxue.v v4, 0(a1), v8, v4
-	vamomaxue.v x0, 0(a1), v8, v4
-	vamomaxue.v v4, 0(a1), v8, v4, v0.t
-	vamomaxue.v x0, 0(a1), v8, v4, v0.t
+	vamominei8.v v4, 0(a1), v8, v4
+	vamominei8.v x0, 0(a1), v8, v4
+	vamominei8.v v4, 0(a1), v8, v4, v0.t
+	vamominei8.v x0, 0(a1), v8, v4, v0.t
+	vamomaxei8.v v4, 0(a1), v8, v4
+	vamomaxei8.v x0, 0(a1), v8, v4
+	vamomaxei8.v v4, 0(a1), v8, v4, v0.t
+	vamomaxei8.v x0, 0(a1), v8, v4, v0.t
+	vamominuei8.v v4, 0(a1), v8, v4
+	vamominuei8.v x0, 0(a1), v8, v4
+	vamominuei8.v v4, 0(a1), v8, v4, v0.t
+	vamominuei8.v x0, 0(a1), v8, v4, v0.t
+	vamomaxuei8.v v4, 0(a1), v8, v4
+	vamomaxuei8.v x0, 0(a1), v8, v4
+	vamomaxuei8.v v4, 0(a1), v8, v4, v0.t
+	vamomaxuei8.v x0, 0(a1), v8, v4, v0.t
+
+	vamoaddei16.v v4, (a1), v8, v4
+	vamoaddei16.v x0, (a1), v8, v4
+	vamoaddei16.v v4, (a1), v8, v4, v0.t
+	vamoaddei16.v x0, (a1), v8, v4, v0.t
+	vamoswapei16.v v4, (a1), v8, v4
+	vamoswapei16.v x0, (a1), v8, v4
+	vamoswapei16.v v4, (a1), v8, v4, v0.t
+	vamoswapei16.v x0, (a1), v8, v4, v0.t
+
+	vamoxorei16.v v4, (a1), v8, v4
+	vamoxorei16.v x0, (a1), v8, v4
+	vamoxorei16.v v4, (a1), v8, v4, v0.t
+	vamoxorei16.v x0, (a1), v8, v4, v0.t
+	vamoandei16.v v4, (a1), v8, v4
+	vamoandei16.v x0, (a1), v8, v4
+	vamoandei16.v v4, (a1), v8, v4, v0.t
+	vamoandei16.v x0, (a1), v8, v4, v0.t
+	vamoorei16.v v4, (a1), v8, v4
+	vamoorei16.v x0, (a1), v8, v4
+	vamoorei16.v v4, (a1), v8, v4, v0.t
+	vamoorei16.v x0, (a1), v8, v4, v0.t
+
+	vamominei16.v v4, (a1), v8, v4
+	vamominei16.v x0, (a1), v8, v4
+	vamominei16.v v4, (a1), v8, v4, v0.t
+	vamominei16.v x0, (a1), v8, v4, v0.t
+	vamomaxei16.v v4, (a1), v8, v4
+	vamomaxei16.v x0, (a1), v8, v4
+	vamomaxei16.v v4, (a1), v8, v4, v0.t
+	vamomaxei16.v x0, (a1), v8, v4, v0.t
+	vamominuei16.v v4, (a1), v8, v4
+	vamominuei16.v x0, (a1), v8, v4
+	vamominuei16.v v4, (a1), v8, v4, v0.t
+	vamominuei16.v x0, (a1), v8, v4, v0.t
+	vamomaxuei16.v v4, (a1), v8, v4
+	vamomaxuei16.v x0, (a1), v8, v4
+	vamomaxuei16.v v4, (a1), v8, v4, v0.t
+	vamomaxuei16.v x0, (a1), v8, v4, v0.t
+
+	vamoaddei16.v v4, 0(a1), v8, v4
+	vamoaddei16.v x0, 0(a1), v8, v4
+	vamoaddei16.v v4, 0(a1), v8, v4, v0.t
+	vamoaddei16.v x0, 0(a1), v8, v4, v0.t
+	vamoswapei16.v v4, 0(a1), v8, v4
+	vamoswapei16.v x0, 0(a1), v8, v4
+	vamoswapei16.v v4, 0(a1), v8, v4, v0.t
+	vamoswapei16.v x0, 0(a1), v8, v4, v0.t
+
+	vamoxorei16.v v4, 0(a1), v8, v4
+	vamoxorei16.v x0, 0(a1), v8, v4
+	vamoxorei16.v v4, 0(a1), v8, v4, v0.t
+	vamoxorei16.v x0, 0(a1), v8, v4, v0.t
+	vamoandei16.v v4, 0(a1), v8, v4
+	vamoandei16.v x0, 0(a1), v8, v4
+	vamoandei16.v v4, 0(a1), v8, v4, v0.t
+	vamoandei16.v x0, 0(a1), v8, v4, v0.t
+	vamoorei16.v v4, 0(a1), v8, v4
+	vamoorei16.v x0, 0(a1), v8, v4
+	vamoorei16.v v4, 0(a1), v8, v4, v0.t
+	vamoorei16.v x0, 0(a1), v8, v4, v0.t
+
+	vamominei16.v v4, 0(a1), v8, v4
+	vamominei16.v x0, 0(a1), v8, v4
+	vamominei16.v v4, 0(a1), v8, v4, v0.t
+	vamominei16.v x0, 0(a1), v8, v4, v0.t
+	vamomaxei16.v v4, 0(a1), v8, v4
+	vamomaxei16.v x0, 0(a1), v8, v4
+	vamomaxei16.v v4, 0(a1), v8, v4, v0.t
+	vamomaxei16.v x0, 0(a1), v8, v4, v0.t
+	vamominuei16.v v4, 0(a1), v8, v4
+	vamominuei16.v x0, 0(a1), v8, v4
+	vamominuei16.v v4, 0(a1), v8, v4, v0.t
+	vamominuei16.v x0, 0(a1), v8, v4, v0.t
+	vamomaxuei16.v v4, 0(a1), v8, v4
+	vamomaxuei16.v x0, 0(a1), v8, v4
+	vamomaxuei16.v v4, 0(a1), v8, v4, v0.t
+	vamomaxuei16.v x0, 0(a1), v8, v4, v0.t
+
+	vamoaddei32.v v4, (a1), v8, v4
+	vamoaddei32.v x0, (a1), v8, v4
+	vamoaddei32.v v4, (a1), v8, v4, v0.t
+	vamoaddei32.v x0, (a1), v8, v4, v0.t
+	vamoswapei32.v v4, (a1), v8, v4
+	vamoswapei32.v x0, (a1), v8, v4
+	vamoswapei32.v v4, (a1), v8, v4, v0.t
+	vamoswapei32.v x0, (a1), v8, v4, v0.t
+
+	vamoxorei32.v v4, (a1), v8, v4
+	vamoxorei32.v x0, (a1), v8, v4
+	vamoxorei32.v v4, (a1), v8, v4, v0.t
+	vamoxorei32.v x0, (a1), v8, v4, v0.t
+	vamoandei32.v v4, (a1), v8, v4
+	vamoandei32.v x0, (a1), v8, v4
+	vamoandei32.v v4, (a1), v8, v4, v0.t
+	vamoandei32.v x0, (a1), v8, v4, v0.t
+	vamoorei32.v v4, (a1), v8, v4
+	vamoorei32.v x0, (a1), v8, v4
+	vamoorei32.v v4, (a1), v8, v4, v0.t
+	vamoorei32.v x0, (a1), v8, v4, v0.t
+
+	vamominei32.v v4, (a1), v8, v4
+	vamominei32.v x0, (a1), v8, v4
+	vamominei32.v v4, (a1), v8, v4, v0.t
+	vamominei32.v x0, (a1), v8, v4, v0.t
+	vamomaxei32.v v4, (a1), v8, v4
+	vamomaxei32.v x0, (a1), v8, v4
+	vamomaxei32.v v4, (a1), v8, v4, v0.t
+	vamomaxei32.v x0, (a1), v8, v4, v0.t
+	vamominuei32.v v4, (a1), v8, v4
+	vamominuei32.v x0, (a1), v8, v4
+	vamominuei32.v v4, (a1), v8, v4, v0.t
+	vamominuei32.v x0, (a1), v8, v4, v0.t
+	vamomaxuei32.v v4, (a1), v8, v4
+	vamomaxuei32.v x0, (a1), v8, v4
+	vamomaxuei32.v v4, (a1), v8, v4, v0.t
+	vamomaxuei32.v x0, (a1), v8, v4, v0.t
+
+	vamoaddei32.v v4, 0(a1), v8, v4
+	vamoaddei32.v x0, 0(a1), v8, v4
+	vamoaddei32.v v4, 0(a1), v8, v4, v0.t
+	vamoaddei32.v x0, 0(a1), v8, v4, v0.t
+	vamoswapei32.v v4, 0(a1), v8, v4
+	vamoswapei32.v x0, 0(a1), v8, v4
+	vamoswapei32.v v4, 0(a1), v8, v4, v0.t
+	vamoswapei32.v x0, 0(a1), v8, v4, v0.t
+
+	vamoxorei32.v v4, 0(a1), v8, v4
+	vamoxorei32.v x0, 0(a1), v8, v4
+	vamoxorei32.v v4, 0(a1), v8, v4, v0.t
+	vamoxorei32.v x0, 0(a1), v8, v4, v0.t
+	vamoandei32.v v4, 0(a1), v8, v4
+	vamoandei32.v x0, 0(a1), v8, v4
+	vamoandei32.v v4, 0(a1), v8, v4, v0.t
+	vamoandei32.v x0, 0(a1), v8, v4, v0.t
+	vamoorei32.v v4, 0(a1), v8, v4
+	vamoorei32.v x0, 0(a1), v8, v4
+	vamoorei32.v v4, 0(a1), v8, v4, v0.t
+	vamoorei32.v x0, 0(a1), v8, v4, v0.t
+
+	vamominei32.v v4, 0(a1), v8, v4
+	vamominei32.v x0, 0(a1), v8, v4
+	vamominei32.v v4, 0(a1), v8, v4, v0.t
+	vamominei32.v x0, 0(a1), v8, v4, v0.t
+	vamomaxei32.v v4, 0(a1), v8, v4
+	vamomaxei32.v x0, 0(a1), v8, v4
+	vamomaxei32.v v4, 0(a1), v8, v4, v0.t
+	vamomaxei32.v x0, 0(a1), v8, v4, v0.t
+	vamominuei32.v v4, 0(a1), v8, v4
+	vamominuei32.v x0, 0(a1), v8, v4
+	vamominuei32.v v4, 0(a1), v8, v4, v0.t
+	vamominuei32.v x0, 0(a1), v8, v4, v0.t
+	vamomaxuei32.v v4, 0(a1), v8, v4
+	vamomaxuei32.v x0, 0(a1), v8, v4
+	vamomaxuei32.v v4, 0(a1), v8, v4, v0.t
+	vamomaxuei32.v x0, 0(a1), v8, v4, v0.t
+
+	vamoaddei64.v v4, (a1), v8, v4
+	vamoaddei64.v x0, (a1), v8, v4
+	vamoaddei64.v v4, (a1), v8, v4, v0.t
+	vamoaddei64.v x0, (a1), v8, v4, v0.t
+	vamoswapei64.v v4, (a1), v8, v4
+	vamoswapei64.v x0, (a1), v8, v4
+	vamoswapei64.v v4, (a1), v8, v4, v0.t
+	vamoswapei64.v x0, (a1), v8, v4, v0.t
+
+	vamoxorei64.v v4, (a1), v8, v4
+	vamoxorei64.v x0, (a1), v8, v4
+	vamoxorei64.v v4, (a1), v8, v4, v0.t
+	vamoxorei64.v x0, (a1), v8, v4, v0.t
+	vamoandei64.v v4, (a1), v8, v4
+	vamoandei64.v x0, (a1), v8, v4
+	vamoandei64.v v4, (a1), v8, v4, v0.t
+	vamoandei64.v x0, (a1), v8, v4, v0.t
+	vamoorei64.v v4, (a1), v8, v4
+	vamoorei64.v x0, (a1), v8, v4
+	vamoorei64.v v4, (a1), v8, v4, v0.t
+	vamoorei64.v x0, (a1), v8, v4, v0.t
+
+	vamominei64.v v4, (a1), v8, v4
+	vamominei64.v x0, (a1), v8, v4
+	vamominei64.v v4, (a1), v8, v4, v0.t
+	vamominei64.v x0, (a1), v8, v4, v0.t
+	vamomaxei64.v v4, (a1), v8, v4
+	vamomaxei64.v x0, (a1), v8, v4
+	vamomaxei64.v v4, (a1), v8, v4, v0.t
+	vamomaxei64.v x0, (a1), v8, v4, v0.t
+	vamominuei64.v v4, (a1), v8, v4
+	vamominuei64.v x0, (a1), v8, v4
+	vamominuei64.v v4, (a1), v8, v4, v0.t
+	vamominuei64.v x0, (a1), v8, v4, v0.t
+	vamomaxuei64.v v4, (a1), v8, v4
+	vamomaxuei64.v x0, (a1), v8, v4
+	vamomaxuei64.v v4, (a1), v8, v4, v0.t
+	vamomaxuei64.v x0, (a1), v8, v4, v0.t
+
+	vamoaddei64.v v4, 0(a1), v8, v4
+	vamoaddei64.v x0, 0(a1), v8, v4
+	vamoaddei64.v v4, 0(a1), v8, v4, v0.t
+	vamoaddei64.v x0, 0(a1), v8, v4, v0.t
+	vamoswapei64.v v4, 0(a1), v8, v4
+	vamoswapei64.v x0, 0(a1), v8, v4
+	vamoswapei64.v v4, 0(a1), v8, v4, v0.t
+	vamoswapei64.v x0, 0(a1), v8, v4, v0.t
+
+	vamoxorei64.v v4, 0(a1), v8, v4
+	vamoxorei64.v x0, 0(a1), v8, v4
+	vamoxorei64.v v4, 0(a1), v8, v4, v0.t
+	vamoxorei64.v x0, 0(a1), v8, v4, v0.t
+	vamoandei64.v v4, 0(a1), v8, v4
+	vamoandei64.v x0, 0(a1), v8, v4
+	vamoandei64.v v4, 0(a1), v8, v4, v0.t
+	vamoandei64.v x0, 0(a1), v8, v4, v0.t
+	vamoorei64.v v4, 0(a1), v8, v4
+	vamoorei64.v x0, 0(a1), v8, v4
+	vamoorei64.v v4, 0(a1), v8, v4, v0.t
+	vamoorei64.v x0, 0(a1), v8, v4, v0.t
+
+	vamominei64.v v4, 0(a1), v8, v4
+	vamominei64.v x0, 0(a1), v8, v4
+	vamominei64.v v4, 0(a1), v8, v4, v0.t
+	vamominei64.v x0, 0(a1), v8, v4, v0.t
+	vamomaxei64.v v4, 0(a1), v8, v4
+	vamomaxei64.v x0, 0(a1), v8, v4
+	vamomaxei64.v v4, 0(a1), v8, v4, v0.t
+	vamomaxei64.v x0, 0(a1), v8, v4, v0.t
+	vamominuei64.v v4, 0(a1), v8, v4
+	vamominuei64.v x0, 0(a1), v8, v4
+	vamominuei64.v v4, 0(a1), v8, v4, v0.t
+	vamominuei64.v x0, 0(a1), v8, v4, v0.t
+	vamomaxuei64.v v4, 0(a1), v8, v4
+	vamomaxuei64.v x0, 0(a1), v8, v4
+	vamomaxuei64.v v4, 0(a1), v8, v4, v0.t
+	vamomaxuei64.v x0, 0(a1), v8, v4, v0.t
 
 	vadd.vv v4, v8, v12
 	vadd.vx v4, v8, a1
