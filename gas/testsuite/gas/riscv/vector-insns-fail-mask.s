268e4f74ce90085c0ad7ccb29b8b9671d9f8b5b7
Nelson Chu <nelson.chu@sifive.com>
2020-08-13 17:27:33 +0800

    RISC-V: Fix the constraints for vector mask and compress instructions.
    Add the missing constraints for vmsbf.m, vmsif.m and vmsof.m.  Their
    destination vector register can not overlap the source register.
    
    Besides, we don't need to check the VM overlap for vcompress.vm, so
    change the match_func from match_vd_neq_vs1_neq_vs2 to
    match_vd_neq_vs1_neq_vs2_neq_vm.
    
    	opcodes/
    	* riscv-opc.c (match_vd_neq_vs1_neq_vs2): New function.  Only
    	check if VD is overlap VS2.
    	(riscv_opcodes): We don't need to check the VM overlap for
    	vcompress.vm, so change the match_func to match_vd_neq_vs1_neq_vs2.
    
    	gas/
    	* testsuite/gas/riscv/vector-insns-fail-mask.l: Add the missing
    	overlap failed cases for vmsbf.m, vmsif.m and vmsof.m.
    	* testsuite/gas/riscv/vector-insns-fail-mask.s: Likewise.

--- gas/testsuite/gas/riscv/vector-insns-fail-mask.s
+++ gas/testsuite/gas/riscv/vector-insns-fail-mask.s
@@ -1,13 +1,16 @@
 # Vector Set-before-first Mask Bit
 
+	vmsbf.m v4, v4		# vd overlap vs2
 	vmsbf.m v0, v4, v0.t	# vd overlap vm
 
 # Vector Set-including-first Mask Bit
 
+	vmsif.m v4, v4		# vd overlap vs2
 	vmsif.m v0, v4, v0.t	# vd overlap vm
 
 # Vector Set-only-first Mask Bit
 
+	vmsof.m v4, v4		# vd overlap vs2
 	vmsof.m v0, v4, v0.t	# vd overlap vm
 
 # Vector Iota Instruction
