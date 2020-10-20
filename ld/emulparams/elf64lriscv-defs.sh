3e933cecd378bbd2823513778337976c5a829223
Nelson Chu <nelson.chu@sifive.com>
2020-10-20 02:16:05 -0700

    RISC-V: Move RO/RW data sections far away from text in default linker script.
    This patch is only used for testing.
    
    If we want to move RO, ideally we only need to set the ld options
    -z separate-code and -Trodata-segment=addr.  But since the option
    -z separate-code doesn't work for us, and I don't know why for now.
    Therefore, we have to define RODATA_ADDR, and set --enable-separate-code
    in the binutils conifgure.
    
    If we want to move RW, then only define the DATA_ADDR should be enough.

--- ld/emulparams/elf64lriscv-defs.sh
+++ ld/emulparams/elf64lriscv-defs.sh
@@ -1,2 +1,8 @@
 source_sh ${srcdir}/emulparams/elf32lriscv-defs.sh
 ELFSIZE=64
+
+# Move RO that far away from text.
+#RODATA_ADDR=0x8000000000
+
+# Move RW that far away from text and RO.
+#DATA_ADDR=0x8000000000
