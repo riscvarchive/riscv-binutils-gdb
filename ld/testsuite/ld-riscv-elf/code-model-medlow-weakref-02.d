e3c7b81a9220fcf586d7992622a2a4575d4d9a7f
Nelson Chu <nelson.chu@sifive.com>
2020-10-26 21:49:01 -0700

    RISC-V: Minor fixs and updates for ld code-model* testcases.
    	ld/
    	* testsuite/ld-riscv-elf/ld-riscv-elf.exp: Add two new weakref
    	testcases for medlow.
    	* testsuite/ld-riscv-elf/code-model-01.ld: Remove PROVIDE keyword
    	to make sure __global_pointer$ is always set for medlow and medany.
    	* testsuite/ld-riscv-elf/code-model-02.ld: Likewise.
    	* testsuite/ld-riscv-elf/code-model.s: Divide data symbols into global
    	and local, and add more patterns to test.
    	* testsuite/ld-riscv-elf/code-model-compact-01.d: Updated.
    	* testsuite/ld-riscv-elf/code-model-compact-02.d: Likewise.
    	* testsuite/ld-riscv-elf/code-model-compact-weakref-01.d: Likewise.
    	* testsuite/ld-riscv-elf/code-model-compact-weakref-02.d: Likewise.
    	* testsuite/ld-riscv-elf/code-model-medany-01.d: Likewise.
    	* testsuite/ld-riscv-elf/code-model-medany-02.d: Likewise.
    	* testsuite/ld-riscv-elf/code-model-medany-weakref-02.d: Likewise.
    	* testsuite/ld-riscv-elf/code-model-medlow-01.d: Likewise.
    	* testsuite/ld-riscv-elf/code-model-medlow-02.d: Likewise.
    	* testsuite/ld-riscv-elf/code-model-medlow-weakref-01.d: New testcase.
    	* testsuite/ld-riscv-elf/code-model-medlow-weakref-02.d: Likewise.

--- ld/testsuite/ld-riscv-elf/code-model-medlow-weakref-02.d
+++ ld/testsuite/ld-riscv-elf/code-model-medlow-weakref-02.d
@@ -1,5 +1,5 @@
 #source: code-model.s
-#as: -march=rv64i -mabi=lp64 --defsym __medany__=1 --defsym __undefweak__=1
+#as: -march=rv64i -mabi=lp64 --defsym __medlow__=1 --defsym __undefweak__=1
 #ld: -Tcode-model-02.ld -melf64lriscv --no-relax
 #objdump: -d -Mno-aliases
 
@@ -10,4 +10,4 @@ Disassembly of section .text:
 
 0+800000000 <_start>:
 [ 	]+[0-9a-f]+:[ 	]+[0-9a-f]+[ 	]+lui[ 	]+t0,0x0
-[ 	]+[0-9a-f]+:[ 	]+[0-9a-f]+[ 	]+ld[ 	]+t0,[0-9]+\(t0\) # [0-9a-f]+ <_GLOBAL_OFFSET_TABLE_\+0x[0-9a-f]+>
+[ 	]+[0-9a-f]+:[ 	]+[0-9a-f]+[ 	]+addi[ 	]+t0,t0,0 # 0 <.*>
--- ld/testsuite/ld-riscv-elf/code-model.s
+++ ld/testsuite/ld-riscv-elf/code-model.s
@@ -5,57 +5,69 @@ _start:
 .ifdef __compact__
 	# Compact prolog.
 .L1:
-	auipc   gp, %pcrel_hi(__global_pointer__)
-	addi    gp, gp, %pcrel_lo(.L1)
-	ld      t0, 0(gp)
-	add     gp, gp, t0
-
-	# I assume we will use PCREL to access the function in the
-	# text section, rather than use GPREL or GOT_GPREL.
-
+	auipc	gp, %pcrel_hi(__global_pointer__)
+	addi	gp, gp, %pcrel_lo(.L1)
+	ld	t0, 0(gp)
+	add	gp, gp, t0
 .ifdef __undefweak__
 	# Refer to undefined weak symbol by GPREL.
 	lui	t0, %gprel_hi(symbolW)
-	add     t0, t0, gp, %gprel(symbolW)
-	addi    t0, t0, %gprel_lo(symbolW)
-	# Refer to data symbol by GOT_GPREL.
-	lui     t0, %got_gprel_hi(symbolW)
-	add     t0, t0, gp, %got_gprel(symbolW)
-	addi    t0, t0, %got_gprel_lo(symbolW)
+	add	t0, t0, gp, %gprel(symbolW)
+	addi	t0, t0, %gprel_lo(symbolW)
+	# Refer to undefined weak symbol by GOT_GPREL.
+	lui	t0, %got_gprel_hi(symbolW)
+	add	t0, t0, gp, %got_gprel(symbolW)
+	ld	t0, %got_gprel_lo(symbolW)(t0)
 .else
-	# Refer to data symbol by GPREL.
-	lui     t0, %gprel_hi(symbolA)
-	add     t0, t0, gp, %gprel(symbolA)
-	addi    t0, t0, %gprel_lo(symbolA)
-	# Refer to data symbol by GOT_GPREL.
-	lui     t0, %got_gprel_hi(symbolA)
-	add     t0, t0, gp, %got_gprel(symbolA)
-	addi    t0, t0, %got_gprel_lo(symbolA)
+	# Refer to local data symbol by GPREL.
+	lui	t0, %gprel_hi(symbolL)
+	add	t0, t0, gp, %gprel(symbolL)
+	addi	t0, t0, %gprel_lo(symbolL)
+	# Refer to global data symbol by GPREL.
+	lui	t0, %gprel_hi(symbolG)
+	add	t0, t0, gp, %gprel(symbolG)
+	addi	t0, t0, %gprel_lo(symbolG)
+	# Refer to local data symbol by GOT_GPREL.
+	lui	t0, %got_gprel_hi(symbolL)
+	add	t0, t0, gp, %got_gprel(symbolL)
+	ld	t0, %got_gprel_lo(symbolL)(t0)
+	# Refer to global data symbol by GOT_GPREL.
+	lui	t0, %got_gprel_hi(symbolG)
+	add	t0, t0, gp, %got_gprel(symbolG)
+	ld	t0, %got_gprel_lo(symbolG)(t0)
 .endif
 .endif
 
 .ifdef __medany__
-.ifdef __undefweak__
-	# Refer to undefined weak symbol by PCREL.
-	la	t0, symbolW
-	# Refer to undefined weak symbol by GOT_PCREL.
 	.option pic
+.ifdef __undefweak__
+	# Refer to undefined weak symbol by GOT_PCREL.
 	la	t0, symbolW
 	.option nopic
 .else
-	# Refer to data symbol by PCREL.
-	la	t0, symbolA
-	# Refer to data symbol by GOT_PCREL.
-	.option pic
-	la	t0, symbolA
+	# Refer to global data symbol by GOT_PCREL.
+	la	t0, symbolG
 	.option nopic
+	# Refer to local data symbol by PCREL.
+	lla	t0, symbolL
+	# Refer to non-pic data global symbol by PCREL.
+	la	t0, symbolG
 .endif
 .endif
 
-.ifdef  __medlow__
-	# Refer to data symbol by absolutely access.
-	lui	t0, %hi(symbolA)
-	addi	t0, t0, %lo(symbolA)
+.ifdef __medlow__
+.ifdef __undefweak__
+	# Refer to undefined weak symbol by absolutely access.
+	lui	t0, %hi(symbolW)
+	addi	t0, t0, %lo(symbolW)
+.else
+	# Refer to local data symbol by absolutely access.
+	lui	t0, %hi(symbolL)
+	addi	t0, t0, %lo(symbolL)
+	# Refer to global data symbol by absolutely access.
+	lui	t0, %hi(symbolG)
+	addi	t0, t0, %lo(symbolG)
+.endif
 .endif
 	.size   _start, .-_start
 
@@ -69,8 +81,11 @@ __global_pointer__:
 .endif
 
 	.data
-symbolA:
-	.dword  0x1122334455667788
+	.global symbolG
+symbolL:
+	.dword	0x1111222233334444
+symbolG:
+	.dword	0x5555666677778888
 
 .ifdef __undefweak__
 	.weak	symbolW
