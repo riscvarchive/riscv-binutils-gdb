f931236d12aa0f0298f34a35f1291ec3d905573d
Nelson Chu <nelson.chu@sifive.com>
2021-02-24 09:52:53 +0800

    RISC-V/rvv: Update the vector spec version back to 1.0-draft-20210130.

--- opcodes/riscv-opc.c
+++ opcodes/riscv-opc.c
@@ -2113,7 +2113,7 @@ const struct riscv_ext_version riscv_ext_version_table[] =
 {"p", ISA_SPEC_CLASS_20190608, 0, 2},
 {"p", ISA_SPEC_CLASS_2P2,      0, 1},
 
-{"v", ISA_SPEC_CLASS_NONE,     0, 10},
+{"v", ISA_SPEC_CLASS_NONE,     1, 0},
 
 {"n", ISA_SPEC_CLASS_20190608, 1, 1},
 {"n", ISA_SPEC_CLASS_2P2,      1, 1},
@@ -2121,8 +2121,8 @@ const struct riscv_ext_version riscv_ext_version_table[] =
 {"zicsr", ISA_SPEC_CLASS_20191213, 2, 0},
 {"zicsr", ISA_SPEC_CLASS_20190608, 2, 0},
 
-{"zvamo",   ISA_SPEC_CLASS_NONE, 0, 10},
-{"zvlsseg", ISA_SPEC_CLASS_NONE, 0, 10},
+{"zvamo",   ISA_SPEC_CLASS_NONE, 1, 0},
+{"zvlsseg", ISA_SPEC_CLASS_NONE, 1, 0},
 
 /* Terminate the list.  */
 {NULL, 0, 0, 0}
